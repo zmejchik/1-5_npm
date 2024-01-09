f5932657a0b5b9f6aa2dbbb4250338f3bb7d6e04
zmejchik <zmejchik@gmail.com>
2024-01-09 17:39:53 +0200

    9

--- docs/assets/scripts/bundle.js
+++ docs/assets/scripts/bundle.js
@@ -9,7 +9,7 @@ function changeMinute(directChangeMinute) {
   if (directChangeMinute === 'plusone') {
     countMinute += 1;
   }
-divLabelMinutes.innerHTML = countMinute;
+  divLabelMinutes.innerHTML = countMinute;
 }
 
 function hiddenAllButtons() {
